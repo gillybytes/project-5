53d772990b3e9c4dc356e9ca991a25ab7c016283
Jinxiang-Lin <78502763+Jinxiang-Lin@users.noreply.github.com>
2021-03-24 16:32:34 -0400

    style for login and signup updated

--- activafy-app/src/App.js
+++ activafy-app/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <Router>
       <div className="App">
         <Switch>
-          <Route exact path='/' component={Home} />
+          <Route exact path='/home' component={Home} />
           <Route exact path='/signup' component={Signup} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/settings' component={Settings}/>
