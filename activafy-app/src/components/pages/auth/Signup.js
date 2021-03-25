0748b5d9af6dad6dd17336227ccb6ccc8f3716d8
Gillian Pantaleon <gillybytes@gmail.com>
2021-03-24 21:05:40 -0400

    added password recovery prompt

--- activafy-app/src/components/pages/auth/Signup.js
+++ activafy-app/src/components/pages/auth/Signup.js
@@ -1,14 +1,20 @@
+import { useState } from 'react'
 import { Menu } from '../../layout/Menu'
-import { Container, Form, Button } from 'react-bootstrap'
+import { Container, Form, Button, Modal } from 'react-bootstrap'
 
 export const Signup = () => {
-    const nav = [
-        {
-          id: 1,
-          link: "/",
-          label: "Login"
-        }
-      ]
+  const nav = [
+    {
+      id: 1,
+      link: "/",
+      label: "Login"
+    }
+  ]
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return(
       <>
         <Menu nav={nav} link='/'/>
@@ -32,10 +38,24 @@ export const Signup = () => {
             <Button variant="primary" href="/home">
               Sign in with Google account
             </Button> { }
-            <Button variant="primary" href="/home">
+            <Button variant="primary" onClick={handleShow}>
               Forgot password?
             </Button>
           </Form>
+
+
+          <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" centered show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Password recovery</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>We sent you a password recovery email.</Modal.Body>
+                <Modal.Footer>
+                 <Button variant="primary" onClick={handleClose}>
+                    Ok
+                  </Button>          
+                </Modal.Footer>
+              </Modal>
+
         </Container>
         
           
