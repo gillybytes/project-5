df6a92cca62a39a1c69de21520e3feb9dfeff839
Gillian Pantaleon <gillybytes@gmail.com>
2021-05-19 23:00:15 -0400

    fixed signup

--- activafy-app/client/src/components/auth/Signup.js
+++ activafy-app/client/src/components/auth/Signup.js
@@ -1,150 +1,125 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { Container, Col, Form, Button, Modal } from 'react-bootstrap'
+import AuthContext from '../../context/auth/authContext';
+import isAuthenticatedUser from '../../utils/isAuthenticated';
 
 import { Menu } from '../layout/Menu'
-import { Container, Col, Form, Button, Modal } from 'react-bootstrap'
-
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { connect } from 'react-redux';
-
 import '../style/Auth.css';
 
-import PropTypes from "prop-types";
-import { registerUser } from "../../actions/authAction";
-import classnames from "classnames";
 
-const nav = [
-  {
-    id: 1,
-    link: "/",
-    label: "Login"
-  }
-]
+const Signup = props => {
+  const authContext = useContext(AuthContext);
 
-class Signup extends Component {
-  constructor(){
-    super();
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      password2: "",
-      errors: {}
-    };
-  }
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({
-        errors: nextProps.errors
-      });
+  const { register, error, clearErrors, isAuthenticated } = authContext;
+  
+  useEffect(() => {
+    if(isAuthenticated || isAuthenticatedUser()) {
+      //props.history.push('/home');
     }
-  }
 
+    if(error === 'User already exists') {
+      clearErrors();
+    }
+  }, [error, isAuthenticated, props.history]);
 
-  onChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
-  onSubmit = e => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const { name, email, password, confirmPassword } = user;
+
+  const handleChange = e => setUser({...user, [e.target.name]: e.target.value});
+
+  const handleSubmit = e => {
     e.preventDefault();
-    const newUser = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password,
-      password2: this.state.password2
-    };
-    this.props.registerUser(newUser, this.props.history); 
+    register({
+      name,
+      email,
+      password
+    });
   };
 
+  const nav = [
+    {
+      id: 1,
+      link: "/",
+      label: "Log in"
+    },
+    {
+      id: 2,
+      link: "/signup",
+      label: "Sign up"
+    }
+  ]
 
-  render(){
-    const { errors } = this.state;
   return(
       <>
-        <Menu nav={nav} link='/'/>
+        <Menu nav={nav} />
 
         <Container className="section">
           <Col class id="signup" className="wrapper">
             <Col className="text-center" xs={6} md={4}>
-              <h3><em>Commit to be fit.</em></h3>
+              <h3><em>Commit to be fit.</em></h3><br/>
               <h5>Create an account.</h5>
-              <Form onSubmit={this.onSubmit}>
-                <Form.Group controlId="formBasicName">
-                  <Form.Label>Name</Form.Label>
-                  <div>
-                    <input
-                      onChange={this.onChange}
-                      value={this.state.name}
-                      error={errors.name}
-                      id="name"
-                      type="text"
-                      className={classnames("", {
-                        invalid: errors.name
-                      })}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                  <Form.Row>
+                    <Form.Control
+                      type='text'
+                      name='name'
+                      placeholder='Name'
+                      value={name}
+                      onChange={handleChange}
+                      required
                     />
-                    <span className="red-text">{errors.name}</span>
-                  </div>
+                  </Form.Row>
                 </Form.Group>
-
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email</Form.Label>
-                  <div>
-                    <input
-                      onChange={this.onChange}
-                      value={this.state.email}
-                      error={errors.email}
-                      id="email"
-                      type="email"
-                      className={classnames("", {
-                        invalid:errors.email
-                      })}
+                <Form.Group>
+                  <Form.Row>
+                    <Form.Control
+                      type='email'
+                      name='email'
+                      placeholder='Email'
+                      value={email}
+                      onChange={handleChange}
+                      required
                     />
-                    <span className="red-text">{errors.email}</span>
-                  </div>
-
+                  </Form.Row>
                 </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <div>
-                    <input
-                      onChange={this.onChange}
-                      value={this.state.password}
-                      error={errors.password}
-                      id="password"
-                      type="password"
-                      className={classnames("", {
-                        invalid: errors.password
-                      })}
+                <Form.Group>
+                  <Form.Row>
+                    <Form.Control
+                      type='password'
+                      name='password'
+                      placeholder='Password'
+                      value={password}
+                      onChange={handleChange}
+                      required
                     />
-                    <span className="red-text">{errors.password}</span>
-                  </div>
+                  </Form.Row>
                 </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Confirm Password</Form.Label>
-                  <div>
-                    <input
-                      onChange={this.onChange}
-                      value={this.state.password2}
-                      error={errors.password2}
-                      id="password2"
-                      type="password"
-                      className={classnames("", {
-                        invalid: errors.password2
-                      })}
+                <Form.Group>
+                  <Form.Row>
+                    <Form.Control
+                      type='password'
+                      name='confirmPassword'
+                      placeholder='Confirm Password'
+                      value={confirmPassword}
+                      onChange={handleChange}
+                      required
                     />
-                    <span className="red-text">{errors.password2}</span>
-                  </div>
+                  </Form.Row>
                 </Form.Group>
-
-                <Button type = "submit">
-                  Sign up
-                </Button>
-                <Button variant="primary" href="/home">
-                  Sign in with Google account
-                </Button>
-                <Button variant="primary" >
-                  Forgot password?
-                </Button>
+                <Form.Group className="text-center">
+                  <Form.Check type='checkbox' label="By checking this box, I agree to the Terms of Service and Privacy Policy." required />
+                </Form.Group>
+                <p>Click here for the <a href="/terms" target="_blank" >Terms of Service</a> & <a href="/privacy" target="_blank"> Privacy Policy</a>.</p>
+                <Button className="btn-custom" type = "submit">Sign up</Button>
+                <Button className="btn-custom" href="/home">Sign up with Google</Button>
+                <h6>Already have an account? {''} <a href='/' className='text-center mb-md-4'>Login here </a></h6>
               </Form>
             </Col>
           </Col>
@@ -160,23 +135,9 @@ class Signup extends Component {
                   </Button>          
                 </Modal.Footer>
               </Modal>
-
         </Container>
       </>
   )
-  }
 }
-Signup.propTypes = {
-  registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
-};
 
-const mapStateToProps = state => ({
-  auth: state.auth,
-  errors: state.errors
-});
-export default connect(
-  mapStateToProps,
-  { registerUser }
-)(withRouter(Signup));
+export default Signup;
